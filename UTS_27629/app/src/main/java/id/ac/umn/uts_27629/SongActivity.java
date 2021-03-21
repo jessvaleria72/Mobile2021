0c3ad8304b98715b94e78b7e2b0f20e9156a27bf
jessvaleria <jessvaleria72@gmail.com>
2021-03-22 00:17:14 +0700

    Create SongActivity.java

---
+++ UTS_27629/app/src/main/java/id/ac/umn/uts_27629/SongActivity.java
@@ -0,0 +1,165 @@
+package id.ac.umn.uts_27629;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class SongActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE = 1;
+    static ArrayList<MusicFiles> musicFiles;
+    static boolean shuffleBoolean = false, repeatBoolean = false;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_song);
+        permission();
+        initViewPager();
+    }
+
+    private void initViewPager() {
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        GridView gridView = findViewById(R.id.gridView);
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragments(new SongsFragment(), "Songs");
+        viewPager.setAdapter(viewPagerAdapter);
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+    }
+
+    public static class ViewPagerAdapter extends FragmentPagerAdapter{
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+
+        public ViewPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+        void addFragments(Fragment fragment, String title){
+            fragments.add(fragment);
+            titles.add(title);
+        }
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.opt_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.menu:
+                Intent profilIntent = new Intent(SongActivity.this, ProfilActivity.class);
+                if(profilIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(profilIntent);
+                }
+                return true;
+            case R.id.logout:
+                Intent logoutIntent = new Intent(SongActivity.this, MainActivity.class);
+                if(logoutIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(logoutIntent);
+                }
+                return true;
+            default: return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void permission(){
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(SongActivity.this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                    , REQUEST_CODE);
+        }
+        else{
+            musicFiles = getAllAudio(this);
+            initViewPager();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_CODE){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+                musicFiles = getAllAudio(this);
+                initViewPager();
+            }else {
+                ActivityCompat.requestPermissions(SongActivity.this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}
+                        , REQUEST_CODE);
+            }
+        }
+    }
+
+    public static ArrayList<MusicFiles> getAllAudio(Context context){
+        ArrayList<MusicFiles> tempAudioList = new ArrayList<>();
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        String[] projection = {
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.DATA,
+                MediaStore.Audio.Media.ARTIST,
+        };
+        Cursor cursor = context.getContentResolver().query(uri, projection,
+                null, null, null);
+        if(cursor != null){
+            while(cursor.moveToNext()){
+                String title = cursor.getString(0);
+                String duration = cursor.getString(1);
+                String path = cursor.getString(2);
+                String artist = cursor.getString(3);
+                MusicFiles musicFiles = new MusicFiles(path, title, artist, duration);
+                tempAudioList.add(musicFiles);
+            }
+            cursor.close();
+        }
+        return tempAudioList;
+    }
+}
