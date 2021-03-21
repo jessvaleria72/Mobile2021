40585d00ff4111760da4f7201c65050fbd01a1f0
jessvaleria <jessvaleria72@gmail.com>
2021-03-22 00:17:18 +0700

    Create ProfilActivity.java

---
+++ UTS_27629/app/src/main/java/id/ac/umn/uts_27629/ProfilActivity.java
@@ -0,0 +1,17 @@
+package id.ac.umn.uts_27629;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ProfilActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil);
+    }
+}
