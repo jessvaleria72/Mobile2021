913d960f3fc1b1ba86b4fd10b3385ac08f35b98b
jessvaleria <jessvaleria72@gmail.com>
2021-04-12 16:41:04 +0700

    innitial commit lage lage

--- UAS/app/src/main/java/id/ac/umn/uas/MainActivity.java
+++ UAS/app/src/main/java/id/ac/umn/uas/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 Button login;
+Button login2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
